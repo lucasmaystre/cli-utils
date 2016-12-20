96a415f17aadd5efe6e0dbad01e2b5f8fac93866
Lucas Maystre <lucas@maystre.ch>
2016-12-20 14:49:31 +0100

    Tidying up `osx.bash`.
    I believe that nowadays theses `LC_*` are not needed anymore, and that they are
    more harmful than anything.

--- bash/osx.bash
+++ bash/osx.bash
@@ -5,10 +5,6 @@
 # Used by `ls`. Replaces `--color=auto` on BSD systems.
 export CLICOLOR=1
 
-# Setting for the new UTF-8 terminal support in Lion
-export LC_CTYPE=en_US.UTF-8
-export LC_ALL=en_US.UTF-8
-
 # Spotlight command line tool. Useful for searching in PDFs.
 alias search="mdfind -onlyin ./"
 # QuickLook debug tool.
