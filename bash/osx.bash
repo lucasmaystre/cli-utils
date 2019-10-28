74373f04a658eef6a200a38148fa6950f396fdf6
Lucas Maystre <lucas@maystre.ch>
2019-10-28 20:27:02 +0000

    Remove bash deprecation warning on macOS Catalina.

--- bash/osx.bash
+++ bash/osx.bash
@@ -5,6 +5,9 @@
 # Used by `ls`. Replaces `--color=auto` on BSD systems.
 export CLICOLOR=1
 
+# Starting with macOS Catalina, bash is deprecated.
+export BASH_SILENCE_DEPRECATION_WARNING=1
+
 # Spotlight command line tool. Useful for searching in PDFs.
 alias search="mdfind -onlyin ./"
 # QuickLook debug tool.
